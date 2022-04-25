ca798a48a130d001d272113e89e84f96a2b5bbf8
Phabiola Mandu <100391057+Phabiola-Mandu@users.noreply.github.com>
2022-04-25 12:48:12 -0300

    Add files via upload

---
+++ pen-export-ZEadwjZ/README.md
@@ -0,0 +1,4 @@
+# Calculadora de média
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/Phabiola-Mandu/pen/ZEadwjZ](https://codepen.io/Phabiola-Mandu/pen/ZEadwjZ).
+
---
+++ pen-export-ZEadwjZ/src/script.js
@@ -0,0 +1,15 @@
+var nome = "Phabiola"
+
+var notaDoPrimieroBimestre = 9
+var notaDoSegundoBimestre = 7
+var notaDoTerceiroBimestre = 4
+var notaDoQuartoBimestre = 2
+
+var notaFinal = (notaDoPrimieroBimestre + notaDoSegundoBimestre + notaDoTerceiroBimestre + notaDoQuartoBimestre) / 4
+
+var notaFixada = notaFinal.toFixed(1)
+
+console.log("Bem vindo " + nome)
+console.log(notaFixada)
+
+// isso foi muito divertido 
